bfbd3ea8b3a212647695aa3e4be5c36ec4a397d9
mukulsomukesh <mukulsomukesh@gmail.com>
2023-08-14 14:56:35 +0530

    style app.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Box, Flex, Heading, useTheme } from '@chakra-ui/react';
+import pic from "./assets/tic-tac-toe.gif";
+import CustomButton from './components/CustomButton';
+import './App.css'; 
 
 function App() {
+  // Get the theme object using the useTheme hook
+  const theme = useTheme(); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex
+      alignItems="center"
+      justifyContent="space-evenly"
+      h="100vh"
+      bg={theme.colors.brand[50]} 
+    >
+      <Box
+        textAlign="center" 
+        width={{ base: "100%", md: "auto" }} 
+        my={{ base: 4, md: 0 }} 
+      >
+        <Heading color={theme.colors.brand[300]}> Welcome To Tic-Tac-Toe </Heading>
+        <CustomButton text="Play With Computer" />
+        <CustomButton text="Play With Friend" />
+      </Box>
+    </Flex>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    50: '#f5efe6',
+    100: '#ff923e',
+    200: '#b39d8f',
+    300: '#0f1624',
+  },
+}
+
+const theme = extendTheme({ colors })
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
